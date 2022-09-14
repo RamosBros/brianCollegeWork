51683abb29538dac2585c52afee2798fdd7b69a2
lemokelw <brianramos892@gmail.com>
2022-09-14 13:07:02 -0400

    reworked the main class, started working on a little dumb program; added comments describing my program to Grades class because i have to for class

--- Main.java
+++ Main.java
@@ -1,11 +1,22 @@
 import src.com.collegework.labone.LabOne;
-
+import java.util.Scanner;
 public class Main {
+    public static Scanner INPUT = new Scanner(System.in);
     public static void main(String[] args) {
+//        doenst work atm cause im dumb but i will get it working soon no kizzy owo
 
-        System.out.println("Hello World!");
+        /* System.out.println("How much do you love me on a scale of one - 10");
+        int userInput = INPUT.nextLine();
+        int x = userInput;
+        if (x < 5) {
+            System.out.println("I know you love me more owo");
+        } else if (x => 5 && x < 50) {
+            System.out.println("come on more than that uwu");
+        } else {
+            System.out.println("finally you're honest");
+        }
 
-        LabOne labOne = new LabOne();
-        labOne.startProgram();
+         */
+
     }
 }
--- src/com/collegework/labone/Grades.java
+++ src/com/collegework/labone/Grades.java
@@ -1,17 +1,24 @@
+/*
+Brian Edgardo Ramos-Leiva
+CIS 1068, Program Design and Abstraction
+9/19/2022
+
+this code takes in several integers from a user (assuming the integers aren't negative)
+and computes their grades according to weights, attendance, and exams.
+ */
 package src.com.collegework.labone;
-
 import java.util.Scanner;
-
 public class Grades {
 
-    public static Scanner CONSOLE = new Scanner(System.in);
+    public static Scanner INPUT = new Scanner(System.in);
 
     public static void main(String[] args) {
+
         System.out.println("Homework weight?");
-        int hwWeight = CONSOLE.nextInt();
+        int hwWeight = INPUT.nextInt();
 
         System.out.println("Exam 1 weight?");
-        int exam1Weight = CONSOLE.nextInt();
+        int exam1Weight = INPUT.nextInt();
         int exam2Weight = 100 - (hwWeight + exam1Weight);
 
         System.out.println("Using weights " + hwWeight + " " + exam1Weight + " " + exam2Weight);
@@ -19,10 +26,10 @@ public class Grades {
         System.out.println("HOMEWORK: ");
 
         System.out.println("Number of homeworks have you had?");
-        int numHW = CONSOLE.nextInt();
+        int numHW = INPUT.nextInt();
 
         System.out.println("What is your average score of all homeworks?");
-        double hwGrade = CONSOLE.nextDouble();
+        double hwGrade = INPUT.nextDouble();
         if (hwGrade < 0) {
             hwGrade = 0;
         } else if (hwGrade > 10) {
@@ -30,10 +37,12 @@ public class Grades {
         }
 
         System.out.println("Number of late days used?");
-        int numLateDays = CONSOLE.nextInt();
+        int numLateDays = INPUT.nextInt();
+
 
         System.out.println("Number of labs you have attended?");
-        int labAttendance = CONSOLE.nextInt();
+        int labAttendance = INPUT.nextInt();
+
 
         double totalPoints = hwCalc(numHW, hwGrade, numLateDays, labAttendance);
         double hwWeightScore = 0;
@@ -41,7 +50,7 @@ public class Grades {
         if (numHW <= 0) {
             totalPoints = 100;
             System.out.println("Total points: " + totalPoints + "/" + totalPoints);
-            hwWeightScore = totalPoints / totalPoints * hwWeight;
+            hwWeightScore = 1.0 * hwWeight;
         } else {
             System.out.println("Total points: " + totalPoints + "/" + (numHW * 10 + numHW * 4));
             hwWeightScore = totalPoints / (numHW * 10 + numHW * 4) * hwWeight;
@@ -50,13 +59,15 @@ public class Grades {
         System.out.println("Weighted Score: " + hwWeightScore);
 
         System.out.println("Exam 1 Score?: ");
-        double exam1Score = CONSOLE.nextDouble();
-        //coniditon B for exam1Score
+        double exam1Score = INPUT.nextDouble();
+
+        //adjusts score according to function examConditionB
         exam1Score = examConiditionB(exam1Score);
 
         //curve
         System.out.println("Curve?");
-        int exam1Curve = CONSOLE.nextInt();
+        int exam1Curve = INPUT.nextInt();
+
         exam1Score += exam1Curve;
 
         if (exam1Score > 100) {
@@ -71,11 +82,13 @@ public class Grades {
         //exam 2 grade
         System.out.println("Weight of exam 2: " + exam2Weight);
         System.out.println("Exam 2 Score?:");
-        double exam2Score = CONSOLE.nextDouble();
+        double exam2Score = INPUT.nextDouble();
+
 
         System.out.println("Curve?");
-        int exam2Curve = CONSOLE.nextInt();
+        int exam2Curve = INPUT.nextInt();
 
+        //adjusts score according to condition see function below
         exam2Score += exam2Curve;
         exam2Score = examConiditionB(exam2Score);
         double exam2WeightScore = exam2Score / 100.0 * exam2Weight;
@@ -92,6 +105,8 @@ public class Grades {
 
     }
 
+    // calculates total points based on number of late days and other factors
+
     public static double hwCalc(int numHW, double hwGrade, int numLateDays, int labAttendance) {
         double totalPoints = hwGrade * numHW;
         if (numLateDays > (numHW) / 2.0) {
@@ -106,6 +121,7 @@ public class Grades {
         return totalPoints;
     }
 
+    // checks if values are below zero or above a hundred and corrects them
     public static double examConiditionB(double examScore) {
         if (examScore < 0) {
             examScore = 0;
