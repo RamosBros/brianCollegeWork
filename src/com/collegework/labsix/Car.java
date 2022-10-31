3759243410ac095a3c4ea247a8a6fa3f7bd0ebb6
lemokelw <brianramos892@gmail.com>
2022-10-31 17:10:59 -0400

    finished part 1a of lab, need to wrte tests; need to finsih part 2a and tests

--- src/com/collegework/labsix/Car.java
+++ src/com/collegework/labsix/Car.java
@@ -1,30 +1,42 @@
 package src.com.collegework.labsix;
-import java.util.Scanner;
-
-
-
 public class Car {
-    Scanner userInput = new Scanner(System.in);
+    private String make;
+    private String model;
+    private int year;
+    private double MPG;
+    private int milesDriven;
+    private int fuelCapacity;
+    private int fuelRemaining;
 
     public Car(String make, String model, int year, double MPG, int milesDriven, int fuelCapacity, int fuelRemaining) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.MPG = MPG;
+        this.milesDriven = milesDriven;
+        this.fuelCapacity = fuelCapacity;
+        this.fuelRemaining = fuelRemaining;
     }
 
-    public double fillTank(double g, int fuelRemaining, int fuelCapacity) {
-        if ((g + fuelRemaining) > fuelCapacity) {
+    public double fillTank(double additionalGas) {
+        double sumOfGas = (additionalGas + fuelRemaining);
+        if (sumOfGas > fuelCapacity) {
             System.out.println("can not add more than car's fuel capacity");
         }
-        fuelRemaining = (int) (g + fuelRemaining);
+        fuelRemaining = (int) sumOfGas;
         return fuelRemaining;
     }
 
-    public double drive(double m, double MPG, int fuelRemaining) {
+    public double drive(double m) {
         double gallonsUsed = (m / MPG);
         fuelRemaining = (int)(fuelRemaining - gallonsUsed);
         return fuelRemaining;
     }
 
-    public String toString(){
-
-        return "0";
+    public String toString() {
+            return make + " " + model + " " + year + " " + MPG + " " + milesDriven + " " + fuelCapacity + " " + fuelRemaining;
+    }
+    public int getFuelRemaining(){
+        return fuelRemaining;
     }
 }
---
+++ test/src/com/collegework/labsix/CarTest.java
@@ -0,0 +1,9 @@
+package src.com.collegework.labsix;
+import src.com.collegework.labsix.Car;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions.*;
+public class CarTest {
+    public void testCar(){
+
+    }
+}
